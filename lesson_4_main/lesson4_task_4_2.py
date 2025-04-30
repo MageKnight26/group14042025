2be2ede81323fcc4230df5e1e00d026cc170760b
Maestro <maestro.dp.ua@gmail.com>
2025-04-30 07:30:08 +0300

    upd 4_2 hometask

--- lesson_4_main/lesson4_task_4_2.py
+++ lesson_4_main/lesson4_task_4_2.py
@@ -7,9 +7,12 @@ all_lists = [numbers_list1, numbers_list2, numbers_list3, numbers_list4]
 for sublist in all_lists:  # processing each list
     if not sublist:
         print(f"{sublist} -> 0")
-        continue
-    sum_even_index = 0
-    for index in range(0, len(sublist), 2):  # we only go through paired index
-        sum_even_index += sublist[index]
-    result = sum_even_index * sublist[-1]  # multiply by the last element of the list
-    print(f"{sublist} => {result}")
+    else:
+        sum_even_index = 0
+        for index, value in enumerate(sublist):  # we only go through paired index
+            if index % 2 == 0:
+                sum_even_index += value
+        result = (
+            sum_even_index * sublist[-1]
+        )  # multiply by the last element of the list
+        print(f"{sublist} => {result}")
